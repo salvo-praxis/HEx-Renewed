b2f763ffcd314f261ba17165fbb4cf47a5a5ec86
Salvo Praxis <105572093+salvo-praxis@users.noreply.github.com>
2023-09-26 13:36:15 -0500

    Restoring Original
    Revert to original

--- classes/Database.class.php
+++ classes/Database.class.php
@@ -1,130 +1,47 @@
 <?php
 class LRSys {
 
-    /**
-     * @var string The name of the user or system.
-     */
     public $name;
-    
-    /**
-     * @var string The username used for authentication.
-     */
     public $user;
-    
-    /**
-     * @var string The password used for authentication.
-     */
     private $pass;
-    
-    /**
-     * @var string The email address of the user.
-     */
     public $email;
-    
-    /**
-     * @var bool Flag to determine whether to keep the session alive.
-     */
     public $keepalive;
-    
-    /**
-     * @var Session The session object for managing user sessions.
-     */
     public $session;
-    
-    /**
-     * @var string The language preference of the user or system.
-     */
     private $lang;
-    
-    /**
-     * @var PDO The PDO object for database interactions.
-     */
     private $pdo;
-    
-    /**
-     * @var mixed The process object or identifier.
-     */
     private $process;
-    
-    /**
-     * @var LogVPC The log object for logging activities or events.
-     */
+
     private $log;
-    
-    /**
-     * @var Ranking The ranking object for managing user rankings.
-     */
     private $ranking;
-    
-    /**
-     * @var Storyline The storyline object for managing the storyline or narrative.
-     */
     private $storyline;
-    
-    /**
-     * @var Clan The clan object for managing user clans or groups.
-     */
     private $clan;
-    
-    /**
-     * @var Mission The mission object for managing user missions or tasks.
-     */
     private $mission;
     
     function __construct() {
-        // Using an autoloader would allow for automatic loading of class files when needed,
-        // reducing the need for manual require statements.
-        // Example of using an autoloader:
-        // spl_autoload_register(function ($class_name) {
-        //     include $class_name . '.class.php';
-        // });
-    
-        // Manually requiring necessary class files.
         require_once 'Session.class.php';
+        $this->pdo = PDO_DB::factory();
+        $this->session = new Session();
         require 'Player.class.php';
         require 'PC.class.php';
         require 'Ranking.class.php';
         require 'Storyline.class.php';
         require 'Clan.class.php';
-    
-        // Initializing the PDO object for database interactions.
-        $this->pdo = PDO_DB::factory();
-    
-        // Initializing the session object for managing user sessions.
-        $this->session = new Session();
-    
-        // Initializing various objects for managing logs, rankings, storylines, and clans.
+
         $this->log = new LogVPC();
         $this->ranking = new Ranking();
         $this->storyline = new Storyline();
         $this->clan = new Clan();
-    
-        // Setting the keepalive property to FALSE by default.
+
         $this->keepalive = FALSE;
+        
     }
 
-    /**
-     * Sets the value of the keepalive property.
-     *
-     * @param bool $keep The value to set the keepalive property to.
-     */
     public function set_keepalive($keep){
-        // Validate the input parameter to ensure it is a boolean value.
-        // This helps in maintaining the integrity of the keepalive property.
-        if (!is_bool($keep)) {
-            // Optionally, throw an exception or trigger an error if the validation fails.
-            // throw new InvalidArgumentException('Parameter $keep must be a boolean value.');
-            trigger_error('Parameter $keep must be a boolean value.', E_USER_WARNING);
-            return;
-        }
-        
-        // Assigning the validated value to the keepalive property.
         $this->keepalive = $keep;
     }
-    
+
     public function register($username, $password, $email)
     {
-        // Validate input parameters and consider using parameterized queries for SQL
         $this->user = $username;
         $this->pass = $password;
         $this->email = $email;
@@ -132,7 +49,6 @@ class LRSys {
         require 'BCrypt.class.php';
         $bcrypt = new BCrypt();
         $hash = $bcrypt->hash(htmlentities($this->pass));
-        // Consider using a more secure method for generating unique IPs
         $gameIP1 = rand(0, 255);
         $gameIP2 = rand(0, 255);
         $gameIP3 = rand(0, 255);
@@ -142,7 +58,6 @@ class LRSys {
         $python = new Python();
         $python->createUser($this->user, $hash, $this->email, $gameIP);
     
-        // Use parameterized queries to prevent SQL injection
         $sql = 'SELECT COUNT(*) AS total, id FROM users WHERE login = :user LIMIT 1';
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute(array(':user' => $this->user));
@@ -157,7 +72,6 @@ class LRSys {
         $finances = new Finances();
         $finances->createAccount($regInfo->id);
     
-        // Use parameterized queries to prevent SQL injection
         $sql = "INSERT INTO stats_register (userID, ip) VALUES (:userID, :ip)";
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute(array(':userID' => $regInfo->id, ':ip' => $_SERVER['REMOTE_ADDR']));
@@ -168,7 +82,6 @@ class LRSys {
     }
 
     private function verifyRegister() {
-        // Consider refactoring this method to reduce complexity and improve readability
         $system = new System();
         
         if(!$system->validate($this->user, 'username')){
@@ -181,7 +94,6 @@ class LRSys {
             return FALSE;
         }
 
-        // Consider simplifying the email validation logic
         if ((strlen(preg_replace('![^A-Z]+!', '', $this->email)) >= 5 && preg_match_all("/[0-9]/", $this->email) >= 2) || preg_match_all("/[0-9]/", $this->email) >= 5){
             $this->session->addMsg(_('Registration complete. You can login now.'), 'notice');
             return FALSE;
@@ -193,7 +105,6 @@ class LRSys {
         }
         
         $this->session->newQuery();
-        // Use parameterized queries to prevent SQL injection
         $sqlQuery = "SELECT email FROM users WHERE login = ? OR email = ? LIMIT 1";
         $sqlLog = $this->pdo->prepare($sqlQuery);
         $sqlLog->execute(array($this->user, $this->email));
@@ -223,12 +134,10 @@ class LRSys {
 
     public function getLastInsertedID()
     {
-        // Consider adding a doc comment to explain the purpose of this method
         return $this->pdo->lastInsertId();
     }
 
     public function login($logUser, $logPass, $special = FALSE) {
-        // Consider refactoring this method to reduce complexity and improve readability
         date_default_timezone_set('UTC');
         $remember = FALSE;
     
@@ -253,7 +162,6 @@ class LRSys {
             require 'BCrypt.class.php';
             $bcrypt = new BCrypt();
             $this->session->newQuery();
-            // Use parameterized queries to prevent SQL injection
             $sqlQuery = "SELECT password, id FROM users WHERE BINARY login = ? LIMIT 1";
             $sqlLog = $this->pdo->prepare($sqlQuery);
             $sqlLog->execute(array($this->user));
@@ -267,7 +175,6 @@ class LRSys {
                     $storyline = $this->storyline;
                     $clan = $this->clan;
     
-                    // Use parameterized queries to prevent SQL injection
                     $sql = "SELECT COUNT(*) AS total FROM users_premium WHERE id = ".$dados['0']['id']." LIMIT 1";
                     $total = $this->pdo->query($sql)->fetch(PDO::FETCH_OBJ)->total;
     
@@ -310,13 +217,11 @@ class LRSys {
     }    
     
     private function loginDatabase($id){
-        // Consider adding a doc comment to explain the purpose of this method
+        
         $this->session->newQuery();
-        // Use parameterized queries to prevent SQL injection
         $sql = 'SELECT COUNT(*) AS total FROM users_online WHERE id = '.$id.' LIMIT 1';
         if($this->pdo->query($sql)->fetch(PDO::FETCH_OBJ)->total > 0){
             $this->session->newQuery();
-            // Use parameterized queries to prevent SQL injection
             $sql = 'DELETE FROM users_online WHERE id = '.$id.' LIMIT 1';
             $this->pdo->query($sql);
         }
@@ -325,15 +230,15 @@ class LRSys {
         $rememberMe = new RememberMe($key, $this->pdo);
         $rememberMe->remember($id, false, $this->keepalive);
         $this->session->newQuery();
-        // Use parameterized queries to prevent SQL injection
         $sql = 'UPDATE users SET lastLogin = NOW() WHERE id = :id';
         $stmt = $this->pdo->prepare($sql);
         $stmt->execute(array(':id' => $id));
         setcookie('logged', '1', time() + 172800);
+        
+        
     }
     
     private function verifyLogin($fb, $tt, $rm) {
-        // Consider adding a doc comment to explain the purpose of this method
         if($fb || $rm || $tt){
             return TRUE;
         }
